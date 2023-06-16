11ef6998abb51cc422bd17eb4f063f866156bcd4
Tarcizo Brito <112122162+tarcizu@users.noreply.github.com>
2023-06-16 18:13:47 -0300

    Completado a classe Cliente

--- src/Entidades/Cliente.java
+++ src/Entidades/Cliente.java
@@ -1,5 +1,27 @@
 package Entidades;
 
 public class Cliente {
+    private String nome;
+    private String email;
+    private int telefone;
+
+    public Cliente(String nome, String email, int telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public int getTelefone() {
+        return this.telefone;
+    }
 
 }
