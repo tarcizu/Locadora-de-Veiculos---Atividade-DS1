65d61e68ecd68489d3c9e8dd989db986fd14df71
Tarcizo Brito <112122162+tarcizu@users.noreply.github.com>
2023-06-30 23:49:08 -0300

    Criei os Arrays de Cliente e Veículos

--- src/App.java
+++ src/App.java
@@ -1,4 +1,14 @@
+import java.util.ArrayList;
+
+import Entidades.Cliente;
+import Entidades.Veiculo;
+
 public class App {
+
+    // Arrys Banco de Dados
+    ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+    ArrayList<Veiculo> veiculos = new ArrayList<Veiculo>();
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
