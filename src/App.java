fe3a739052d9f48c56542b6f858d214ebd5d9e90
Tarcizo Brito <112122162+tarcizu@users.noreply.github.com>
2023-07-01 00:19:04 -0300

    Criei o toString e o metodo alugar do Veiculo

--- src/App.java
+++ src/App.java
@@ -5,14 +5,23 @@ import Entidades.Veiculo;
 
 public class App {
 
-    // Arrys Banco de Dados
-    ArrayList<Cliente> clientes = new ArrayList<Cliente>();
-    ArrayList<Veiculo> veiculos = new ArrayList<Veiculo>();
-
     public static void main(String[] args) throws Exception {
+        // Arrys Banco de Dados
+        ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+        ArrayList<Veiculo> veiculos = new ArrayList<Veiculo>();
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
+        veiculos.add(Veiculo.cadastrar("Ford", "Ka", 1, 10000.0));
+        veiculos.add(Veiculo.cadastrar("Audi", "A3", 2, 10000.0));
+        veiculos.add(Veiculo.cadastrar("Tesla", "Model S", 3, 10000.0));
+        veiculos.get(0).alugar();
+        for (Veiculo carro : veiculos) {
+            System.out.print(carro);
+
+        }
+
     }
+
     // Testando
 }
--- src/Entidades/Veiculo.java
+++ src/Entidades/Veiculo.java
@@ -3,48 +3,72 @@ package Entidades;
 public class Veiculo {
     private String marca;
     private String modelo;
-    private String categoria;
+    private int categoria;
     private boolean statusLocacao;
     private double KMRodado;
 
-    public Veiculo(String marca, String modelo, String categoria, boolean statusLocacao, double KMRodado) {
+    public Veiculo(String marca, String modelo, int categoria, double KMRodado) {
         this.marca = marca;
         this.modelo = modelo;
         this.categoria = categoria;
-        this.statusLocacao = statusLocacao;
+        this.statusLocacao = true;
         this.KMRodado = KMRodado;
     }
 
     public String getMarca() {
         return marca;
     }
+
     public void setMarca(String marca) {
         this.marca = marca;
     }
+
     public String getModelo() {
         return modelo;
     }
+
     public void setCambio(String modelo) {
         this.modelo = modelo;
     }
-    public String getCategoria() {
+
+    public int getCategoria() {
         return categoria;
     }
-    public void setCategoria(String categoria) {
+
+    public void setCategoria(int categoria) {
         this.categoria = categoria;
     }
+
     public boolean getStatusLocacao() {
         return statusLocacao;
     }
+
     public void setStatusLocacao(boolean statusLocacao) {
         this.statusLocacao = statusLocacao;
     }
+
     public double getKMRodado() {
         return KMRodado;
     }
+
     public void setLugares(double KMRodado) {
         this.KMRodado = KMRodado;
-    }    
+    }
 
+    public static Veiculo cadastrar(String marca, String modelo, int categoria, Double km) {
+
+        Veiculo carro = new Veiculo(marca, modelo, categoria, km);
+        return carro;
+    }
+
+    public void alugar() {
+        this.statusLocacao = false;
+    }
+
+    public String toString() {
+        return String.format("Marca: %s\nModelo: %s\nCategoria: %d\nKilometragem: %.0f\nDisponibilidade: %s\n\n",
+                this.marca, this.modelo, this.categoria, this.KMRodado,
+                this.statusLocacao == true ? "Disponivel" : "Alugado");
+    }
 
 }
